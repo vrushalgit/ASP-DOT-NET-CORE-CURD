908a720826e8ddd9fb77d23c9db438ee0233a741
VD <vrushalsd19@gmail.com>
2022-10-20 23:22:23 +0530

    Adding new Project

---
+++ First-ASP-Dot-Net-Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace First_ASP_Dot_Net_Project.Data
+{
+    public class ApplicationDbContext:DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+    }
+}
